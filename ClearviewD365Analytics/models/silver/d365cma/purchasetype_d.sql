7388c726771e5acf218b703229ab4739f3623ec0
Vijayan <Vishnu.Vijayan@crowe.com>
2025-11-17 14:30:54 -0500

    Add purchasetype dimension

---
+++ ClearviewD365Analytics/models/silver/d365cma/purchasetype_d.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table', tags=['silver'], alias='purchasetype_dim') }}
+
+-- External table: silver.cma_PurchaseType
+-- Provides the lookup values for Purchase Type across purchase models.
+
+SELECT PurchaseTypeKey
+     , PurchaseTypeID
+     , PurchaseType
+     , CURRENT_TIMESTAMP AS _CreatedDate
+     , CURRENT_TIMESTAMP AS _ModifiedDate
+  FROM silver.cma_PurchaseType;
