bc42a02c9f5040645a02972e999f11d11145c8c8
Vijayan <Vishnu.Vijayan@crowe.com>
2025-11-17 15:33:18 -0500

    Use bronze enumeration in inventory reference type

--- ClearviewD365Analytics/models/silver/d365cma/inventoryreferencetype_d.sql
+++ ClearviewD365Analytics/models/silver/d365cma/inventoryreferencetype_d.sql
@@ -1,4 +1,13 @@
 {{ config(materialized='table', tags=['silver'], alias='inventoryreferencetype') }}
 
-SELECT *
-  FROM silver.cma_InventoryReferenceType;
+WITH detail AS (
+    SELECT e1.EnumValueID AS InventoryReferenceTypeID
+         , e1.EnumValue   AS InventoryReferenceType
+      FROM {{ ref('enumeration') }} e1
+     WHERE e1.Enum = 'InventRefType'
+)
+
+SELECT InventoryReferenceTypeID
+     , InventoryReferenceType
+  FROM detail
+ ORDER BY InventoryReferenceTypeID;
