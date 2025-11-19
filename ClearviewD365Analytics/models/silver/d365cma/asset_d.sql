d38066805992f1ea6a7304a0ae638285ddbe3049
Vijayan <Vishnu.Vijayan@crowe.com>
2025-11-19 13:17:05 -0500

    Format asset model

--- ClearviewD365Analytics/models/silver/d365cma/asset_d.sql
+++ ClearviewD365Analytics/models/silver/d365cma/asset_d.sql
@@ -6,54 +6,51 @@
 -- external_table_name: AssetDetail
 -- schema_name: temp
 
-WITH
-assetstage AS (
-    SELECT OT.dataareaid                                                         AS LegalEntityID
-             , OT.objectid                                                            AS AssetID
-             , ISNULL (NULLIF(OT.name, ''), OT.objectid)                              AS Asset
-             , AOT.objecttypeid                                                       AS AssetType
-             , OT.modelyear                                                           AS ModelYear
-             , M.modelid                                                              AS Model
-             , OLS.objectlifecyclestateid                                             AS AssetState
-             , OT.fixedassetid                                                        AS FixedAssetID
-             , OT.wrkctrid                                                            AS ResourceID
-             , FL.functionallocationid                                                AS FunctionalLocationID
-             , CAST(OT.activefrom AS DATE)                                            AS AssetValidFrom
-             , ISNULL (NULLIF(CAST(OT.activeto AS DATE), '1900-01-01'), '9999-12-31') AS AssetValidTo
-             , OT.recid                                                            AS _RecID
-             , 1                                                                      AS _SourceID
-
-          FROM {{ ref('entassetobjecttable') }}               OT
-
-          LEFT JOIN {{ ref('entassetobjectlifecyclestate') }} OLS
-            ON OLS.recid = OT.objectlifecyclestate
-          LEFT JOIN {{ ref('entassetfunctionallocation') }}   FL
-            ON FL.recid  = OT.functionallocation
-          LEFT JOIN {{ ref('entassetobjecttype') }}           AOT
-            ON AOT.recid = OT.objecttype
-          LEFT JOIN {{ ref('entassetmodel') }}                M
-            ON M.recid   = OT.model;
+WITH assetstage AS (
+    SELECT
+        OT.dataareaid AS LegalEntityID,
+        OT.objectid AS AssetID,
+        ISNULL(NULLIF(OT.name, ''), OT.objectid) AS Asset,
+        AOT.objecttypeid AS AssetType,
+        OT.modelyear AS ModelYear,
+        M.modelid AS Model,
+        OLS.objectlifecyclestateid AS AssetState,
+        OT.fixedassetid AS FixedAssetID,
+        OT.wrkctrid AS ResourceID,
+        FL.functionallocationid AS FunctionalLocationID,
+        CAST(OT.activefrom AS DATE) AS AssetValidFrom,
+        ISNULL(NULLIF(CAST(OT.activeto AS DATE), '1900-01-01'), '9999-12-31') AS AssetValidTo,
+        OT.recid AS _RecID,
+        1 AS _SourceID
+    FROM {{ ref('entassetobjecttable') }} AS OT
+    LEFT JOIN {{ ref('entassetobjectlifecyclestate') }} AS OLS
+        ON OLS.recid = OT.objectlifecyclestate
+    LEFT JOIN {{ ref('entassetfunctionallocation') }} AS FL
+        ON FL.recid = OT.functionallocation
+    LEFT JOIN {{ ref('entassetobjecttype') }} AS AOT
+        ON AOT.recid = OT.objecttype
+    LEFT JOIN {{ ref('entassetmodel') }} AS M
+        ON M.recid = OT.model
 )
-SELECT ROW_NUMBER() OVER (ORDER BY ts._RecID, ts._SourceID) AS AssetKey
-          , afl.AssetFunctionalLocationKey
-         , ts.LegalEntityID
-         , ts.AssetID
-         , ts.Asset
-         , ts.AssetState
-         , ts.FixedAssetID
-		 , ts.ResourceID
-         , ts.AssetType
-         , ts.Model
-         , ts.ModelYear
-         , ts.AssetValidFrom
-         , ts.AssetValidTo
-         , ts._RecID
-         , ts._SourceID
-
-         ,  cast(CURRENT_TIMESTAMP as DATETIME2(6))    AS  _CreatedDate
-         ,  cast(CURRENT_TIMESTAMP as DATETIME2(6))    AS  _ModifiedDate
-      FROM assetstage                           ts
-      LEFT JOIN {{ ref('assetfunctionallocation_d') }} afl
-        ON afl.LegalEntityID        = ts.LegalEntityID
-       AND afl.FunctionalLocationID = ts.FunctionalLocationID;
-
+SELECT
+    ROW_NUMBER() OVER (ORDER BY ts._RecID, ts._SourceID) AS AssetKey,
+    afl.AssetFunctionalLocationKey,
+    ts.LegalEntityID,
+    ts.AssetID,
+    ts.Asset,
+    ts.AssetState,
+    ts.FixedAssetID,
+    ts.ResourceID,
+    ts.AssetType,
+    ts.Model,
+    ts.ModelYear,
+    ts.AssetValidFrom,
+    ts.AssetValidTo,
+    ts._RecID,
+    ts._SourceID,
+    cast(CURRENT_TIMESTAMP AS DATETIME2(6)) AS _CreatedDate,
+    cast(CURRENT_TIMESTAMP AS DATETIME2(6)) AS _ModifiedDate
+FROM assetstage AS ts
+LEFT JOIN {{ ref('assetfunctionallocation_d') }} AS afl
+    ON afl.LegalEntityID = ts.LegalEntityID
+   AND afl.FunctionalLocationID = ts.FunctionalLocationID;
