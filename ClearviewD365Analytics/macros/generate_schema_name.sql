0013777c98949dfb418e07353c90180bec50a4b9
Vijayan <Vishnu.Vijayan@crowe.com>
2025-11-19 12:46:16 -0500

    Fix schema generator implementation

--- ClearviewD365Analytics/macros/generate_schema_name.sql
+++ ClearviewD365Analytics/macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-  {% if custom_schema_name is none %}
-    return(target.schema)
-  {% else %}
-    return(custom_schema_name)   -- e.g., 'bronze', 'silver', 'gold'
-  {% endif %}
-{% endmacro %}
+{% macro generate_schema_name(custom_schema_name=none, node=none) -%}
+  {%- if custom_schema_name is none or custom_schema_name == '' -%}
+    {{ target.schema }}
+  {%- else -%}
+    {{ custom_schema_name }}
+  {%- endif -%}
+{%- endmacro %}
